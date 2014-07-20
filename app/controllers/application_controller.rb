1b2d27076b8ba1bed3f90428528334db3ca97492
Jim Posen <jim.posen@gmail.com>
2014-07-20 11:14:47 -0700

    Redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :redirect
+
+  def redirect
+    if request != 'localhost'
+      redirect_to "http://54.210.58.4"
+      return
+    end
+  end
 end
