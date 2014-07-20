413b6153a33afe5e4f10a8e240286bfe7fba948f
Michael Lai <themichaellai@gmail.com>
2014-07-20 05:32:50 -0700

    Login endpoint fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,7 +21,8 @@ class HomeController < ApplicationController
   def log_in
     email = params[:email]
     password = params[:password]
-    if User.find_by_email(email).valid_password?(password)
+    user = User.find_by_email(email)
+    if user and user.valid_password?(password)
       render status: 200, json: "Yes"
     else
       render status: 403, json: "No"
